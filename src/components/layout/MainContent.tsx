95d5c445884c3f8bb3392defa223175afc0cee40
Grzegorz Wilk <grzegorzwilk178@gmail.com>
2024-02-23 08:10:09 +0100

    weather app

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,10 +1,11 @@
 import TodoList from "./todo-list/TodoList"
+import WeatherApp from "./weather-app/WeatherApp"
 
 export default function MainContent(){
     return(
         <div className="container pt-10 columns-2">
-
             <TodoList />
+            <WeatherApp />
         </div>
     )
 }

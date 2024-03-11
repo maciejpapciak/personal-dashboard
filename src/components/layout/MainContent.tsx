c90503ec11f9f0c0459239009c3ead30fbf4732b
Maciek Papciak <59439684+maciejpapciak@users.noreply.github.com>
2024-03-11 16:30:34 +0100

    Revert "weather app"
    This reverts commit 95d5c445884c3f8bb3392defa223175afc0cee40.

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,11 +1,10 @@
 import TodoList from "./todo-list/TodoList"
-import WeatherApp from "./weather-app/WeatherApp"
 
 export default function MainContent(){
     return(
         <div className="container pt-10 columns-2">
+
             <TodoList />
-            <WeatherApp />
         </div>
     )
 }
--- src/components/layout/weather-app/utils/weatherHelpers.ts
+++
@@ -1,8 +0,0 @@
-
-    const date = new Date()
-    const daysAsString = date.toLocaleString('en', {day: '2-digit'})
-    const monthAsString = date.toLocaleString('en', {month: 'long'})
-
-    const current = `${monthAsString} ${daysAsString}`
-
-    export default current
